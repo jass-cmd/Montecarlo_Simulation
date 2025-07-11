c5530e331a6e9adca361274b5cf3d8408bc16b06
Jassiel <jass.t93@gmail.com>
2025-07-11 00:15:50 -0300

    classe test completed

--- classes/test_risk.py
+++ classes/test_risk.py
@@ -11,6 +11,21 @@ class TestRisk (unittest.TestCase): #esta clase hereda de unittest.testcase los 
         self.assertEqual(risk.name, "Client delay")
         self.assertEqual(risk.probability, 0.3)
         self.assertEqual(risk.impact, 0.5)
+    
+
+    def test_invalid_prob(self):
+        with self.assertRaises(ValueError):
+            Risk("Bad Risk", 1.5, 0.2)
+
+    def test_invalid_type(self):
+        with self.assertRaises(TypeError): 
+            Risk("Dependencies", "high", 0.8)
+
+    def test_invalid_name(self):
+        with self.assertRaises(TypeError):
+            Risk("", 0.9, 0.7)
+
+
 
 if __name__ == '__main__':
     unittest.main()
