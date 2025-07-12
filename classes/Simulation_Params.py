2f6564f3661bc0974e900375cbc4bee55c628eda
Jassiel <jass.t93@gmail.com>
2025-07-12 01:21:35 -0300

    code refactored

--- classes/Simulation_Params.py
+++ classes/Simulation_Params.py
@@ -1,54 +1,76 @@
-class SimulationParameters:
-    
-    """
-    Represents the input parameters required to run the simulation.
-    it is designed to validate user input and support the integration with a frontend application.
-    """
+class SimulationParameters: 
 
-    def __init__(self, backlog: int, th_min: int, th_ex: int, th_max: int, num_sim: int):
-        # Apply rounding and type casting
-        self.backlog = int(round(backlog))
-        self.th_min = int(round(th_min))
-        self.th_ex = int(round(th_ex))
-        self.th_max = int(round(th_max))
-        self.num_sim = int(round(num_sim))
+    def __init__(self, backlog:int, th_min:int, th_ex:int, th_max:int, num_sim:int):
 
-        # Run validations
-        self._validate()
-
-    def _validate(self):
-        if not (self.th_min < self.th_ex < self.th_max):
-            raise ValueError("Throughput values must follow th_min < th_ex < th_max")
-        
-        for attr in ["backlog", "th_min", "th_ex", "th_max", "num_sim"]:
-            value = getattr(self, attr)
-            if value <= 0:
-                raise ValueError(f"{attr} must be greater than 0")
-
-    def __str__(self):
-        return f"SimulationParameters(backlog={self.backlog}, th_min={self.th_min}, th_ex={self.th_ex}, th_max={self.th_max}, num_sim={self.num_sim})"
-
-    def __repr__(self):
-        return f"{self.backlog}, {self.th_min}, {self.th_ex}, {self.th_max}, {self.num_sim}"
-
-    def to_dict(self) -> dict:
-        return {
-            "backlog": self.backlog,
-            "th_min": self.th_min,
-            "th_ex": self.th_ex,
-            "th_max": self.th_max,
-            "num_sim": self.num_sim
-        }
+                SimulationParameters._data_validation (backlog, th_min, th_ex, th_max, num_sim)
+                    
+                self.backlog = backlog
+                self.th_min = th_min
+                self.th_ex = th_ex
+                self.th_max = th_max
+                self.num_sim = num_sim
 
     @staticmethod
-    def from_dict(data: dict) -> "SimulationParameters":
-        try:
-            return SimulationParameters(
-                backlog=data["backlog"],
-                th_min=data["th_min"],
-                th_ex=data["th_ex"],
-                th_max=data["th_max"],
-                num_sim=data["num_sim"]
-            )
-        except KeyError as e:
-            raise ValueError(f"Missing required field: {e.args[0]}")
+    def _data_validation(backlog:int, th_min:int, th_ex:int, th_max:int, num_sim:int):
+                        
+                tempdict = {
+
+                    "backlog":backlog,
+                    "th_min":th_min,
+                    "th_ex":th_ex,
+                    "th_max":th_max,
+                    "num_sim":num_sim
+                }
+            
+                for key, value in tempdict.items():
+                    if not isinstance(value, int):
+                        raise TypeError(f"{key} must be an integer")
+                    
+                    if value <= 0:
+                        raise ValueError(f"{key} must be greater than 0")
+                    
+                if not (th_min < th_ex < th_max):
+                            raise ValueError ("Throughput values must follow th_min < th_ex < th_max")
+
+    def to_dict(self): 
+                return {
+                    "backlog": self.backlog,
+                    "th_min": self.th_min,
+                    "th_ex": self.th_ex,
+                    "th_max": self.th_max,
+                    "num_sim": self.num_sim
+                }
+
+    @staticmethod
+    def from_dict(data: dict):
+                        
+                required_fields= ["backlog","th_min","th_ex","th_max","num_sim"]
+                for x in required_fields:
+                        if x not in data:
+                            raise ValueError(f"Missing required field {x}")
+                        
+                return SimulationParameters(
+
+                        data["backlog"],
+                        data["th_min"],
+                        data["th_ex"],
+                        data["th_max"],
+                        data["num_sim"]
+                    )
+                        
+    def __str__(self):
+            return f"SimulationParameters(backlog={self.backlog},th_min={self.th_min},th_ex={self.th_ex},th_max={self.th_max},num_sim={self.num_sim})"
+    
+    def __repr__(self):
+            return f"{self.backlog},{self.th_min},{self.th_ex},{self.th_max},{self.num_sim}"
+                    
+
+
+
+
+
+
+                        
+                    
+
+                    
