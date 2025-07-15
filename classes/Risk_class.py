c38b4a5e62464251707fae2c4a522d08dba211df
Jassiel <jass.t93@gmail.com>
2025-07-15 15:36:45 -0300

    risk class refactored

--- classes/Risk_class.py
+++ classes/Risk_class.py
@@ -1,11 +1,6 @@
-class Risk:
-    """
-    This class's purpose is to model risks that can affect a team's productivity.
-    It also includes data validation methods to make sure the object is fully validated 
-    and usable by other classes such as the Simulator class.
+class Risk: 
 
-    """
-    def __init__(self, risk_name:str , probability:float, impact:float):
+    def __init__(self, risk_name: str, probability: float, impact: float):
 
         Risk._data_validation(risk_name, probability, impact)
 
@@ -15,69 +10,72 @@ class Risk:
 
     @staticmethod
     def _data_validation(risk_name, probability, impact):
-        
-        if not isinstance (risk_name, str) or not risk_name.strip():
-            raise TypeError ("Risk name must a non empty string")
-        
-        parameters = {"probability": probability, "impact": impact}
-        for key, value in parameters.items():
-            if not (0 < value < 1):
-                raise ValueError (f"The field {key} has to be a value between 0 and 1 (eg. 0.4 --> 40%)")
-        
 
-    def to_dict(self): 
-        return {
-
-        "risk_name": self.risk_name,
-        "probability": self.probability,
-        "impact": self.impact
-
-        }
+        if risk_name is None or not isinstance(risk_name, str) or not risk_name.strip(): 
+                raise TypeError("This field must be a string and must not be empty")
+        
+        risk_parameters = {"probability":probability, "impact":impact}
     
+        for field, value in risk_parameters.items():
+            
+            if value is None or not (0 < value < 1):
+                raise ValueError(f"Field {field} must be values between 0 and 1 (eg. 0.40 ---> 40%)")
+            
     @staticmethod
-    def from_dict(data:dict):
-       try:
-            return Risk (
+    def from_dict(data: dict):
+
+        dict_keys = ["risk_name","probability","impact"]
+        for keys in  dict_keys:
+             if keys not in data:
+                  raise KeyError(f"The key {keys} is missing")
+        
+             value = data[keys]
+             if value is None:
+                  raise ValueError(f"The field {keys} can't be none")
+             
+             if isinstance (value, str) and not value.strip():
+                  raise ValueError(f"the field {keys} cannot be an empty string")
+             
+        return Risk (
+             
             data["risk_name"],
             data["probability"],
             data["impact"]
         )
-
-       except KeyError as e:
-              raise ValueError (f"The field {e.args[0]} is missing the input data")
     
-    def parse (data): 
-        """
-        The user can enter one or more risks,
-        this method parses a dictionary or a list of them into a object or
-        list of objects. After validating structure and content.
+    def to_dict(self):
+         
+         return {
+              "risk_name":self.risk_name,
+              "probability":self.probability,
+              "impact": self.impact
+         }
+    
+    @staticmethod
+    def parse(data):
+         
+         if isinstance(data, dict):
+              return [Risk.from_dict(data)]
         
-        """
-        if isinstance(data, dict): 
-            return [Risk.from_dict(data)]
-        
-        elif isinstance(data, list):
-           
+         elif isinstance(data, list):
             risks = []
             for i, item in enumerate(data):
-                if not isinstance(item, dict):
-                    raise TypeError (f"Error at index {i}. This is not a dictionary")
-                
-                try:
-                    obj = Risk.from_dict(item)
-                    risks.append(obj)
-                except ValueError as e:
-                    raise ValueError (f"Error at index {i}. {e}")
-                
+                 if not isinstance(item, dict):
+                      raise TypeError(f"Error at index {i}. This is not a dictionary")
+                 
+                 try:
+                      obj=Risk.from_dict(item)
+                      risks.append(obj)
+                 except ValueError as e:
+                      raise ValueError(f"Error at index {i}. {e}")
+            
             return risks
-        else:
-            raise TypeError ("Input must be a dictionary or a list of dictionaries.")
-    
-        
+         else:
+              raise TypeError("This input must be a dictionary or a list of dictionaries.")
+         
+
     def __str__(self):
-        return f"Risk (risk_name={self.risk_name}, probability={self.probability}, impact={self.impact})"
+         return f"(Risk={self.risk_name}, probability={self.probability}, impact={self.impact})"
     
     def __repr__(self):
-        return f"({self.risk_name},{self.probability},{self.impact})"
-
-    
+         return f"({self.risk_name}, {self.probability}, {self.impact})"
