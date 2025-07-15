eacd0f86fbb5a80630b907dd4c46570a7f208ce1
Jassiel <jass.t93@gmail.com>
2025-07-15 13:20:58 -0300

    Parameters class refactored

--- classes/Simulation_Params.py
+++ classes/Simulation_Params.py
@@ -1,82 +1,66 @@
 class SimulationParameters: 
 
-    """
-    This class's purpose is to validate the simulation parameters data placed by the users
-    The goal is to create a final object fully validated and ready to be used by the simulator
-    or other parts of the system.
-    
-    """
     def __init__(self, backlog:int, th_min:int, th_ex:int, th_max:int, num_sim:int):
+        
+        SimulationParameters._data_validation(backlog, th_min, th_ex, th_max, num_sim)
 
-                SimulationParameters._data_validation (backlog, th_min, th_ex, th_max, num_sim)
-                    
-                self.backlog = backlog
-                self.th_min = th_min
-                self.th_ex = th_ex
-                self.th_max = th_max
-                self.num_sim = num_sim
-
+        self.backlog = backlog
+        self.th_min = th_min
+        self.th_ex = th_ex
+        self.th_max = th_max
+        self.num_sim = num_sim
+    
     @staticmethod
-    def _data_validation(backlog:int, th_min:int, th_ex:int, th_max:int, num_sim:int):
-                        
-                tempdict = { #temporary dict to validate data
+    def _data_validation(backlog, th_min, th_ex, th_max, num_sim):
 
-                    "backlog":backlog,
-                    "th_min":th_min,
-                    "th_ex":th_ex,
-                    "th_max":th_max,
-                    "num_sim":num_sim
-                }
-            
-                for key, value in tempdict.items():
-                    if not isinstance(value, int):
-                        raise TypeError(f"{key} must be an integer")
-                    
-                    if value <= 0:
-                        raise ValueError(f"{key} must be greater than 0")
-                    
-                if not (th_min < th_ex < th_max):
-                            raise ValueError ("Throughput values must follow th_min < th_ex < th_max")
+         tempdict = {
+             
+             "backlog":backlog,
+             "th_min":th_min,
+             "th_ex":th_ex,
+             "th_max":th_max,
+             "num_sim":num_sim
+        }
+         
+         for key, value in tempdict.items():
+             if value is None or not isinstance(value, int):
+                 raise TypeError(f"The field {key} must be an Integer")
+             
+             if (value <= 0): 
+                 raise ValueError(f"{key} cannot be less or equal zero")
+                 
+         if not (th_min < th_ex < th_max):
+            raise ValueError("Throughput must follow th_min < th_ex < th_max")
+         
+    @staticmethod
+    def from_dict(data:dict): 
+
+        keys = ["backlog", "th_min", "th_ex", "th_max", "num_sim"]
+
+        for x in keys:
+            if x not in data:
+                raise ValueError(f"Field {x} is missing")
+        
+        return SimulationParameters (
+
+            data["backlog"],
+            data["th_min"],
+            data["th_ex"],
+            data["th_max"],
+            data["num_sim"]
+        )
 
     def to_dict(self): 
-                return {
-                    "backlog": self.backlog,
-                    "th_min": self.th_min,
-                    "th_ex": self.th_ex,
-                    "th_max": self.th_max,
-                    "num_sim": self.num_sim
-                }
-
-    @staticmethod
-    def from_dict(data: dict):
-                        
-                required_fields= ["backlog","th_min","th_ex","th_max","num_sim"]
-                for x in required_fields:
-                        if x not in data:
-                            raise ValueError(f"Missing required field {x}")
-                        
-                return SimulationParameters(
-
-                        data["backlog"],
-                        data["th_min"],
-                        data["th_ex"],
-                        data["th_max"],
-                        data["num_sim"]
-                    )
-                        
+        return {
+            "backlog":self.backlog,
+            "th_min":self.th_min,
+            "th_ex":self.th_ex,
+            "th_max":self.th_max,
+            "num_sim":self.num_sim
+        }
+    
     def __str__(self):
-            return f"SimulationParameters(backlog={self.backlog},th_min={self.th_min},th_ex={self.th_ex},th_max={self.th_max},num_sim={self.num_sim})"
+        return (f"SimulationParameters(backlog={self.backlog}, th_min={self.th_min}, th_ex={self.th_ex}, th_max={self.th_max}, num_sim={self.num_sim})")
     
     def __repr__(self):
-            return f"{self.backlog},{self.th_min},{self.th_ex},{self.th_max},{self.num_sim}"
-                    
-
-
-
-
-
-
-                        
-                    
-
-                    
+        return (f"{self.backlog}, {self.th_min}, {self.th_ex}, {self.th_max}, {self.num_sim}")
