2cf0059104a91db3b30636a3714029fc2caa477f
Jassiel <jass.t93@gmail.com>
2025-07-21 21:38:15 -0300

    changes on simulator

--- src/classes/simulator.py
+++ src/classes/simulator.py
@@ -3,80 +3,62 @@ from simulation_params import SimulationParameters
 from risk_class import Risk
 
 class Simulator:
-    """
-    Monte Carlo simulation of project delivery time based on throughput(obj) and risks(obj).
-    """
-    def __init__(self, parameters:SimulationParameters, risks: list[Risk]):
-        
-           self.parameters = parameters
-           self.risks = risks
-           self.results = None 
 
     """
-    Saves the initial state of the object.
-    Creates the atributes of the obj (parameters andn risks)
-    Self results will save the results of the simulation. 
-    This will later be used for data visualization, convergence and sensibility analysis.
-
+    This class is the core class of the whole app.
+    It's purpose is to take the inputs coming from SimulationParameters and Risk
+    and perform the simulation following a triangular distribution probability.
+    It is important to note that in this model the risks acumulates.
     """
-    def run_simulation(self, max_weeks: int = 200):
+
+    def __init__(self, parameters: SimulationParameters, risks: list[Risk]):
+
+        self.parameters = parameters
+        self.risks = risks
+        self.results = None
+
+    def run_simulation(self, max_weeks: int = 150) -> np.ndarray:
 
         """
-        Unpack the values that will be used in the simulation
+        Takes the user input parameters and run the simulation to get the throughput base.
+        No risks applied for now. The output is an ndarray of throughput values.
         """
         backlog = self.parameters.backlog
-        num_sim = self.parameters.num_sim
         th_min = self.parameters.th_min
         th_ex = self.parameters.th_ex
         th_max = self.parameters.th_max
+        num_sim = self.parameters.num_sim
 
-        
-        """
-        Run the simulation to get a matrix of throughput values not adjusted by risks.
-        It's already and ndarray so vectorizaed operations can be used.
-        """
-        base_throughput = np.random.triangular(
-            
+        th_base = np.random.triangular(
             left=th_min,
             mode=th_ex,
             right=th_max,
             size=(num_sim, max_weeks)
-            
-            )
-        
-        #Apply risks to the whole ndarray of throughput values
-        adjusted_throughput = self._apply_risks_vectorized(base_throughput)
+        )
 
-        #Get the cumulative throughput per row (axis = 1)
-        cumulative_throughput = np.cumsum(adjusted_throughput, axis=1)
+        th_adjusted = self._apply_risks(th_base) #apply risks
+        th_accumulated = np.cumsum(th_adjusted, axis=1) #acumulates the throughput along the rows.
+        th_reached = th_accumulated >= backlog #creates a boolean ndarray (mask)
+        self.results = np.argmax(th_reached, axis=1) + 1 #finds the week where th_accumulated = backlog
 
-        #Creates a boolean matrix (mask) to identify the when the simulation hit the target backlog for the first time
-        reached = cumulative_throughput >= backlog #it uses broadcasting to apply the operator to the whole matrix
-        first_success = np.argmax(reached, axis=1) + 1  # +1 because of 0-based index
-        
-
-        self.results = first_success
         return self.results
 
-    def _apply_risks_vectorized(self, throughput: np.ndarray) -> np.ndarray:
-        """
-        Applies risks across the entire matrix using broadcasting and masks.
-        """
-        adjusted = throughput.copy()
+    def _apply_risks(self, th_base_matrix: np.ndarray) -> np.ndarray:
 
+        """
+        Apply risks to the throughput base not before creating a copy of it 
+        for safely data manipulation
+        """
+        adjusted = th_base_matrix.copy()
         for risk in self.risks:
-            #createas a boolean matrix (mask) to determine when the risk occurs
-            risk_occurs = np.random.rand(*throughput.shape) < risk.probability
-
-            #apply risks to base throughput through the mask for everr risk added by the user (warning: it accumulates risks)
-            adjusted = np.where(risk_occurs, adjusted * risk.impact, adjusted)
-
+            risk_mask = np.random.rand(*th_base_matrix.shape) < risk.probability
+            adjusted = np.where(risk_mask, adjusted * risk.impact, adjusted) #apply risks using the mask as filter
         return adjusted
 
     def get_results(self) -> np.ndarray:
-
-        #just for safety, it requires the simulation to run before getting the results.
+        """
+        This is to make sure the simulation is run before trying to pull the results.
+        """
         if self.results is None:
-            raise RuntimeError("Simulation has not been run yet.")
+            raise RuntimeError("The simulation has not been run yet")
         return self.results
-    
